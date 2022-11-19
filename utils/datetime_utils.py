2c7eb908a6ed8946032db3defe9732c58aebb3dc
qiankun <zqkntu@gmail.com>
2022-11-19 22:39:38 +0800

    format code

--- utils/datetime_utils.py
+++ utils/datetime_utils.py
@@ -1,12 +1,10 @@
-# coding:utf-8
 from datetime import datetime
 
 
 def time_delta(t: datetime):
-    """ 计算现在到指定时间的间隔 """
-    dt = datetime.now()-t
-    hours = dt.seconds//3600
-    minutes = (dt.seconds-hours*3600) // 60
-    seconds = dt.seconds % 60
+    d = datetime.now()-t
+    hours = d.seconds//3600
+    minutes = (d.seconds-hours*3600) // 60
+    seconds = d.seconds % 60
     return f'{hours:02}:{minutes:02}:{seconds:02}'
 
