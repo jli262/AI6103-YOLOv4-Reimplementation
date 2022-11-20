a97f892514cf2e1192215695878b8a33e333ff56
lijia <lijiayi262@gmail.com>
2022-11-20 23:41:52 +0800

    update many files

--- evals.py
+++ evals.py
@@ -54,6 +54,7 @@ ax.plot(iterations, mAPs)
 ax.set(xlabel='iteration', ylabel='mAP', title='mAP curve')
 plt.show()
 
+
 model_path = 'model/2022-11-15_13-49-27/Yolo_200.pth'
 image_path = 'VOC2007_test/JPEGImages/007447.jpg'
 
