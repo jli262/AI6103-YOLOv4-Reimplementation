fc88b95b1125f2f1b7c85f96a237440324295c50
lijia <lijiayi262@gmail.com>
2022-11-20 20:28:38 +0800

    update train

--- train.py
+++ train.py
@@ -2,39 +2,45 @@
 from net import TrainPipeline, VOCDataset
 from utils.augmentation_utils import YoloAugmentation, ColorAugmentation
 
+anchor = [[[142, 110], [192, 243], [459, 401]], [[36, 75], [76, 55], [72, 146]], [[12, 16], [19, 36], [40, 28]],]
 
 # train config
 config = {
     "n_classes": len(VOCDataset.VOC2007_classes),
-    "image_size": 416,
-    "anchors": [
-        [[142, 110], [192, 243], [459, 401]],
-        [[36, 75], [76, 55], [72, 146]],
-        [[12, 16], [19, 36], [40, 28]],
-    ],
     "darknet_path": "CSPdarknet53.pth",
-    "lr": 1e-2,
-    "batch_size": 4,
+    "image_size": 416,
     "freeze_batch_size": 8,
     "freeze": True,
-    "freeze_epoch": 0,
+    "freeze_epoch": 50,
+    "anchors": anchor,
+    "lr": 1e-2,
+    "batch_size": 4,
     "max_epoch": 200,
-    "start_epoch": 50,
+    "start_epoch": 0,
     "num_workers": 4
 }
 
+def loadDataset():
+    root = 'VOC2007_train'
+    datasetDefine(root)
+
+
+def datasetDefine(root):
+    global dataset
+    dataset = VOCDataset(
+        root,
+        'train',
+        transformer=YoloAugmentation(config['image_size']),
+        colorTransformer=ColorAugmentation(config['image_size']),
+        keepDifficult=True,
+        ifMosaic=True,
+        ifMixup=True,
+        imageSize=config["image_size"]
+    )
+
+
 # load dataset
-root = 'VOC2007_train'
-dataset = VOCDataset(
-    root,
-    'train',
-    transformer=YoloAugmentation(config['image_size']),
-    colorTransformer=ColorAugmentation(config['image_size']),
-    keepDifficult=True,
-    ifMosaic=True,
-    ifMixup=True,
-    imageSize=config["image_size"]
-)
+loadDataset()
 
 if __name__ == '__main__':
     train_pipeline = TrainPipeline(dataset=dataset, **config)
